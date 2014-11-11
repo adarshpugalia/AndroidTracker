abed05cf0f0058467813c3080d10bfc800d95210
achiever202 <es12b1001@iith.ac.in>
2014-11-11 16:37:06 +0530

    Registered user on server side.

---
+++ php/register.php
@@ -0,0 +1,52 @@
+<?php
+
+   $server = 'localhost';
+   $username = 'root';
+   $password = '123';
+   $database = 'android_tracker';
+
+   $phone = "";
+   $name = "";
+   $GCM_ID = "";
+
+   /* creating connection to the database. */
+   $connection = mysqli_connect($server, $username, $password, $database);
+
+   /* checking for successful connection. */
+   if(!$connection)
+      die("Connection failed: " . mysqli_connect_error());
+
+   /* TO-D0 change it to post request. */
+   /* if post request. */
+   if($_GET)
+   {
+      /* getting the details of the user from the POST request. */
+      $phone = $_GET['Phone'];
+      $name = $_GET['Name'];
+      $GCM_ID = $_GET['GCM_ID'];
+      $password = $_GET['Password'];
+
+      /* TO-DO this code does not work, fix it. */
+      /* check if the phone is already registered. */
+      $sql_query = "SELECT * FROM users WHERE phone='$phone' LIMIT 1";
+      $result = mysqli_query($connection, $sql_query);
+
+      if(!$result)
+         die("Error registering user: " . mysqli_error($connection));
+
+      if(mysql_num_rows($result)>0)
+         echo "Error registring user: Phone number already registered.";
+
+      /* registering the new user. */
+      $sql_query = "INSERT INTO users (phone, GCM_ID, name, password)
+                     VALUES ('$phone', '$GCM_ID', '$name', '$password')";
+
+      if(mysqli_query($connection, $sql_query))
+         echo "Success!";
+      else
+         die("Error registering user: " . mysqli_error($connection));
+
+   } 
+
+   mysqli_close($connection);
+?>
