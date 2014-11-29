1d04ac49c3428350ffc87513a595c08c24c7103d
achiever202 <es12b1001@iith.ac.in>
2014-11-29 13:27:09 +0530

    Obtained contact name from contact number.

--- src/adarsh/awesomeapps/androidtracker/ContactDetails.java
+++ src/adarsh/awesomeapps/androidtracker/ContactDetails.java
@@ -57,4 +57,53 @@ public class ContactDetails
 			cursor.close();
 		return contacts;
 	}
+	
+	String getContactName(String contactNumber)
+	{
+		String contactName = "";
+		
+		/* getting the entire contact list. */
+		getAllContacts();
+		
+		while(cursor.moveToNext())
+		{
+			if(contactNumber.equals(getFormattedContact(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)))))
+			{
+				contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+				break;
+			}
+		}
+		
+		if(cursor!=null && !cursor.isClosed())
+			cursor.close();
+		
+		return contactName;
+	}
+	
+	/* This function builds a formatted 10 digit contact number from the contact string provided. */
+	String getFormattedContact(String contact)
+	{
+		/* if the number is less than 10 digits, not valid. */
+		if(contact.length()<10)
+			return "";
+		
+		String formattedContact = "";
+		for(int i=contact.length()-1, j=0; i>=0 && j<10; i--)
+		{
+			if(contact.charAt(i)>='0' && contact.charAt(i)<='9')
+			{
+				formattedContact += contact.charAt(i);
+				j++;
+			}
+		}
+		
+		if(formattedContact.length()<10)
+			return "";
+		
+		/* The number should begin with a number other than 0. */
+		if(formattedContact.charAt(9)=='0')
+			return "";
+		
+		return new StringBuilder(formattedContact).reverse().toString();
+	}
 }
